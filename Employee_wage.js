2922b65e1757a576b813815d35cc8acc2d5b7f0e
Marvanparsh <panwarpawan507@gmail.com>
2025-03-22 11:17:58 +0530

    UC4 added

--- Employee_wage.js
+++ Employee_wage.js
@@ -37,3 +37,34 @@ empWage = 80;
 empHrs = 4;   
 WAGE_PER_HOUR = 20;   
 console.log("Emp Wage: " + empWage);  
+
+// <==============UC3-Work Hours=================================>
+    function getWorkingHours(empCheck) {
+        switch (empCheck) {
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
+    }
+    
+    empHrs = 0;
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = getWorkingHours(empCheck);
+    empWage = empHrs * WAGE_PER_HOUR;
+    console.log("Emp Wage: " + empWage);
+
+
+// <===================UC4-WagesForMonth======================>
+    let NUM_OF_WORKING_DAYS = 2;
+ empHrs = 0;
+
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+
+ empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hrs: " + empHrs + " Emp Wage: " + empWage);
